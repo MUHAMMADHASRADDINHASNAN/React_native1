0db5251d045473370e280a3bdd811112f44db0c3
MUHAMMADHASRADDINHASNAN <105841107722@student.unismuh.ac.id>
2025-07-24 15:15:17 +0800

    tugaz-4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "BitcountPropSingle_roman-reguler": require("../assets/fonts/BitcountPropSingle_Roman-Regular.ttf"),
+    "NotoSansJP-Regular": require("../assets/fonts/NotoSansJP-Regular.ttf"),
+    "PlaywriteHU-Regular": require("../assets/fonts/PlaywriteHU-Regular.ttf"),
+    "IntelOneMono-Regular": require("../assets/fonts/IntelOneMono-Regular.ttf"),
+    "SpecialGothicExpandedOne-Regular": require("../assets/fonts/SpecialGothicExpandedOne-Regular.ttf"),
+    "WinkyRough-Regular": require("../assets/fonts/WinkyRough-Regular.ttf"),
+    "NotoSansKR-Regular": require("../assets/fonts/NotoSansKR-Regular.ttf"),
+    "PlaywritePL-Regular": require("../assets/fonts/PlaywritePL-Regular.ttf"),
+    "Inconsolata_ExtraExpanded-Regular": require("../assets/fonts/Inconsolata_ExtraExpanded-Regular.ttf"),
+    "BebasNeue-Regular": require("../assets/fonts/BebasNeue-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded) return null;
+
   return <Stack />;
 }
